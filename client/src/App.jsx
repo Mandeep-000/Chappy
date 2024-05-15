4621d2b5d0bf590164de23fcf9e3666ddbe65875
mandeep-000 <p.mandeep0402@gmail.com>
2024-05-15 21:01:56 +0530

    proxy added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import {UserContextProvider} from "./UserContext";
 import Routes from "./Routes";
 
 function App() {
-  axios.defaults.baseURL = 'http://localhost:4040/api/v1';
+  axios.defaults.baseURL = '/api/v1';
   axios.defaults.withCredentials = true;
   return (
     <UserContextProvider>
